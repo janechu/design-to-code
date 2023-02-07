6927b2d123dbf6d379d0c62dba3011386f53c10f
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-02-07 15:18:50 -0800

    Convert the textarea control from class to function (#10)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change converts the textarea React class component to function component.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue converting React classes to React function components

--- packages/design-to-code-react/src/form/controls/control.textarea.tsx
+++ packages/design-to-code-react/src/form/controls/control.textarea.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextareaControlProps } from "./control.textarea.props";
 import { classNames } from "@microsoft/fast-web-utilities";
 import { isDefault } from "./utilities/form";
@@ -9,91 +9,67 @@ import style from "./control.textarea.style.css";
 cssVariables;
 style;
 
-export interface TextareaControlState {
-    isFocused: boolean;
-}
-
 /**
  * Form control definition
  */
-class TextareaControl extends React.Component<
-    TextareaControlProps,
-    TextareaControlState
-> {
-    public static displayName: string = "TextareaControl";
+function TextareaControl(props: TextareaControlProps) {
+    const [focused, setFocus] = useState(false);
 
-    constructor(props: TextareaControlProps) {
-        super(props);
-
-        this.state = {
-            isFocused: false,
-        };
+    function getRows(): number {
+        return typeof props.rows === "number" ? props.rows : 3;
     }
 
-    public render(): React.ReactNode {
-        return (
-            <textarea
-                className={classNames(
-                    "dtc-textarea-control",
-                    ["dtc-textarea-control__disabled", this.props.disabled],
-                    [
-                        "dtc-textarea-control__default",
-                        isDefault(this.props.value, this.props.default),
-                    ]
-                )}
-                id={this.props.dataLocation}
-                name={this.props.dataLocation}
-                rows={this.getRows()}
-                value={this.getValue()}
-                onChange={this.handleChange()}
-                disabled={this.props.disabled}
-                ref={this.props.elementRef as React.Ref<HTMLTextAreaElement>}
-                onFocus={this.handleFocus}
-                onBlur={this.handleBlur}
-                required={this.props.required}
-            />
-        );
-    }
-
-    private getRows(): number {
-        return typeof this.props.rows === "number" ? this.props.rows : 3;
-    }
-
-    private getValue(): string {
+    function getValue(): string {
         // Return undefined to allow typing anywhere other than the end of the string
         // when the typing is occuring in the textarea
-        if (this.state.isFocused) {
+        if (focused) {
             return;
         }
 
-        return typeof this.props.value === "string"
-            ? this.props.value
-            : typeof this.props.default === "string"
-            ? this.props.default
+        return typeof props.value === "string"
+            ? props.value
+            : typeof props.default === "string"
+            ? props.default
             : "";
     }
 
-    private handleFocus = (e: React.FocusEvent<HTMLTextAreaElement>): void => {
-        this.props.reportValidity();
+    function handleFocus(e: React.FocusEvent<HTMLTextAreaElement>): void {
+        props.reportValidity();
 
-        this.setState({
-            isFocused: true,
-        });
-    };
+        setFocus(true);
+    }
 
-    private handleBlur = (e: React.FocusEvent<HTMLTextAreaElement>): void => {
-        this.props.updateValidity();
+    function handleBlur(e: React.FocusEvent<HTMLTextAreaElement>): void {
+        props.updateValidity();
 
-        this.setState({
-            isFocused: false,
-        });
-    };
+        setFocus(false);
+    }
 
-    private handleChange = (): ((e: React.ChangeEvent<HTMLTextAreaElement>) => void) => {
+    function handleChange(): (e: React.ChangeEvent<HTMLTextAreaElement>) => void {
         return (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
-            this.props.onChange({ value: e.target.value });
+            props.onChange({ value: e.target.value });
         };
-    };
+    }
+
+    return (
+        <textarea
+            className={classNames(
+                "dtc-textarea-control",
+                ["dtc-textarea-control__disabled", props.disabled],
+                ["dtc-textarea-control__default", isDefault(props.value, props.default)]
+            )}
+            id={props.dataLocation}
+            name={props.dataLocation}
+            rows={getRows()}
+            value={getValue()}
+            onChange={handleChange()}
+            disabled={props.disabled}
+            ref={props.elementRef as React.Ref<HTMLTextAreaElement>}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            required={props.required}
+        />
+    );
 }
 
 export { TextareaControl };
