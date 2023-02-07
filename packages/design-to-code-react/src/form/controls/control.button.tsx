406c18d818a3326296744f3e036fcca68a888c7b
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-02-07 12:01:19 -0800

    Convert button control in react from class to functional component (#4)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change:
    - Converts the React button control to a functional component
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Convert more controls from class to functional react components

--- packages/design-to-code-react/src/form/controls/control.button.tsx
+++ packages/design-to-code-react/src/form/controls/control.button.tsx
@@ -15,52 +15,15 @@ style;
 
 /**
  * Form control definition
- * @extends React.Component
  */
-class ButtonControl extends React.Component<ButtonControlProps, {}> {
-    public static displayName: string = "ButtonControl";
-
-    public render(): React.ReactNode {
-        return (
-            <React.Fragment>
-                <button
-                    className={classNames(
-                        "dtc-button-control dtc-common-input",
-                        ["dtc-button-control__disabled", this.props.disabled],
-                        [
-                            "dtc-button-control__default dtc-common-default-font",
-                            isDefault(this.props.value, this.props.default),
-                        ]
-                    )}
-                    ref={this.props.elementRef as React.Ref<HTMLButtonElement>}
-                    onBlur={this.props.updateValidity}
-                    onFocus={this.props.reportValidity}
-                    onClick={this.handleButtonClick()}
-                    disabled={this.props.disabled}
-                >
-                    Set to null
-                </button>
-                <input
-                    id={this.props.dataLocation}
-                    hidden={true}
-                    value={this.props.value || ""}
-                    onChange={this.handleInputChange}
-                    disabled={this.props.disabled}
-                    required={this.props.required}
-                />
-            </React.Fragment>
-        );
-    }
-
-    private handleButtonClick = (): ((
-        e: React.MouseEvent<HTMLButtonElement>
-    ) => void) => {
+function ButtonControl(props: ButtonControlProps & React.PropsWithChildren) {
+    function handleButtonClick(): (e: React.MouseEvent<HTMLButtonElement>) => void {
         return (e: React.MouseEvent<HTMLButtonElement>): void => {
             e.preventDefault();
 
-            this.props.onChange({ value: null });
+            props.onChange({ value: null });
         };
-    };
+    }
 
     /**
      * Dummy onChange handler
@@ -69,7 +32,37 @@ class ButtonControl extends React.Component<ButtonControlProps, {}> {
      * therefore a value and onChange handler must still be supplied
      * even if there is no intention to update the value.
      */
-    private handleInputChange = (): void => {};
+    function handleInputChange(): void {}
+
+    return (
+        <React.Fragment>
+            <button
+                className={classNames(
+                    "dtc-button-control dtc-common-input",
+                    ["dtc-button-control__disabled", props.disabled],
+                    [
+                        "dtc-button-control__default dtc-common-default-font",
+                        isDefault(props.value, props.default),
+                    ]
+                )}
+                ref={props.elementRef as React.Ref<HTMLButtonElement>}
+                onBlur={props.updateValidity}
+                onFocus={props.reportValidity}
+                onClick={handleButtonClick()}
+                disabled={props.disabled}
+            >
+                Set to null
+            </button>
+            <input
+                id={props.dataLocation}
+                hidden={true}
+                value={props.value || ""}
+                onChange={handleInputChange}
+                disabled={props.disabled}
+                required={props.required}
+            />
+        </React.Fragment>
+    );
 }
 
 export { ButtonControl };
