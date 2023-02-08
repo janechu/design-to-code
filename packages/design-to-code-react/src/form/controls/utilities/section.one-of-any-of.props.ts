0116d9f74978ef78568499243df63cedc82160e4
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-02-08 10:32:24 -0800

    Convert OneOfAnyOf class to function component (#14)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change converts the OneOfAnyOf React class component to a function component.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue converting React class to function components

--- packages/design-to-code-react/src/form/controls/utilities/section.one-of-any-of.props.ts
+++ packages/design-to-code-react/src/form/controls/utilities/section.one-of-any-of.props.ts
@@ -2,6 +2,7 @@ export interface SectionOneOfAnyOfProps {
     label: string;
     activeIndex: number;
     onUpdate: (activeIndex: number) => void;
+    children: React.ReactNode;
 }
 
 export interface SectionOneOfAnyOfClassNameContract {
