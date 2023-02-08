0847866cc8ca6940b23910f0dab0b4108667d356
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-02-08 12:35:52 -0800

    Convert the viewer class component to a function component (#18)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change converts the viewer from a React class component to a function component.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Continue converting React class components to function components

--- packages/design-to-code-react/src/viewer/viewer/viewer.base.tsx
+++ packages/design-to-code-react/src/viewer/viewer/viewer.base.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { get } from "lodash-es";
 import { canUseDOM } from "exenv-es6";
 import rafThrottle from "raf-throttle";
@@ -10,318 +10,193 @@ import {
 } from "./viewer.props";
 import { MessageSystemType, Register } from "design-to-code";
 
-export interface ViewerState {
-    /**
-     * Signifies if the content region is currently being resized
-     */
-    resizing: boolean;
-
-    /**
-     * The y-position where users started resizing the content region
-     */
-    dragReferenceY: number;
-
-    /**
-     * The x-position where users started resizing the content region
-     */
-    dragReferenceX: number;
-
-    /**
-     * The drag handle location
-     */
-    dragHandleLocation: ResizeHandleLocation;
-}
-
-export class Viewer extends React.Component<
-    ViewerHandledProps & ViewerUnhandledProps,
-    ViewerState
-> {
-    public static displayName: string = "Viewer";
-
-    protected handledProps: ViewerHandledProps = {
-        iframeSrc: void 0,
-        height: void 0,
-        width: void 0,
-        responsive: void 0,
-        messageSystem: void 0,
-        preview: void 0,
+export function Viewer(props: ViewerHandledProps & ViewerUnhandledProps) {
+    let iframeRef: React.RefObject<HTMLIFrameElement>;
+    const [resizing, setResizing] = useState(false);
+    const [dragReferenceY, setDragReferenceY] = useState(null);
+    const [dragReferenceX, setDragReferenceX] = useState(null);
+    const [dragHandleLocation, setDragHandleLocation] = useState(null);
+    const messageSystemConfig: Register = {
+        onMessage: handleMessageSystem,
     };
 
-    private messageSystemConfig: Register;
+    if (props.messageSystem !== undefined) {
+        props.messageSystem.add(messageSystemConfig);
+    }
 
-    private iframeRef: React.RefObject<HTMLIFrameElement>;
+    iframeRef = React.createRef();
+    const throttledHandleMouseMove = rafThrottle(handleMouseMove);
 
-    constructor(props: ViewerHandledProps) {
-        super(props);
+    useEffect(() => {
+        document.addEventListener("mouseup", handleMouseUp);
+        document.addEventListener("mousemove", throttledHandleMouseMove);
 
-        this.state = {
-            resizing: false,
-            dragReferenceY: null,
-            dragReferenceX: null,
-            dragHandleLocation: null,
+        return function cleanup() {
+            document.removeEventListener("mouseup", handleMouseUp);
+            document.removeEventListener("mousemove", throttledHandleMouseMove);
         };
+    });
 
-        this.messageSystemConfig = {
-            onMessage: this.handleMessageSystem,
-        };
-
-        if (props.messageSystem !== undefined) {
-            props.messageSystem.add(this.messageSystemConfig);
-        }
-
-        this.iframeRef = React.createRef();
-        this.handleMouseMove = rafThrottle(this.handleMouseMove);
-    }
-
-    /**
-     * Handle when component updates
-     */
-    public componentDidUpdate(
-        prevProps: ViewerHandledProps,
-        prevState: ViewerState
-    ): void {
-        if (canUseDOM()) {
-            this.shouldUpdateResizingEventListeners(prevState);
-        }
-    }
-
-    public componentWillUnmount(): void {
-        if (this.props.messageSystem !== undefined) {
-            this.props.messageSystem.remove(this.messageSystemConfig);
-        }
-
-        if (canUseDOM() && get(this.iframeRef, "current.contentWindow")) {
-            this.iframeRef.current.contentWindow.removeEventListener(
-                "message",
-                this.handleIframeMessage
-            );
-        }
-    }
-
-    public componentDidMount(): void {
-        if (canUseDOM() && get(this.iframeRef, "current.contentWindow")) {
-            this.iframeRef.current.contentWindow.addEventListener(
-                "message",
-                this.handleIframeMessage
-            );
-        }
-    }
-
-    public render(): JSX.Element {
-        return (
-            <div className={"dtc-viewer"}>
-                <div
-                    className={this.generateContentRegionClassNames()}
-                    style={{
-                        ...this.getHeight(),
-                        ...this.getWidth(),
-                    }}
-                >
-                    {this.renderResponsiveLeftHandle()}
-                    <base target="_blank" />
-                    <iframe
-                        ref={this.iframeRef}
-                        className={"dtc-viewer_iframe"}
-                        src={this.props.iframeSrc}
-                    >
-                        Your browser does not support iframes.
-                    </iframe>
-                    {this.renderResponsiveRightHandle()}
-                    {this.renderResponsiveBottomRow()}
-                </div>
-            </div>
-        );
-    }
-
-    private renderResponsiveLeftHandle(): JSX.Element {
-        if (this.props.responsive) {
+    function renderResponsiveLeftHandle(): JSX.Element {
+        if (props.responsive) {
             return (
                 <button
                     className={`dtc-viewer-handle dtc-viewer-handle__left`}
-                    onMouseDown={this.handleMouseDown(ResizeHandleLocation.left)}
+                    onMouseDown={handleMouseDown(ResizeHandleLocation.left)}
                 />
             );
         }
     }
 
-    private renderResponsiveRightHandle(): JSX.Element {
-        if (this.props.responsive) {
+    function renderResponsiveRightHandle(): JSX.Element {
+        if (props.responsive) {
             return (
                 <button
                     className={`dtc-viewer-handle dtc-viewer-handle__right`}
-                    onMouseDown={this.handleMouseDown(ResizeHandleLocation.right)}
+                    onMouseDown={handleMouseDown(ResizeHandleLocation.right)}
                 />
             );
         }
     }
 
-    private renderResponsiveBottomRow(): JSX.Element {
-        if (this.props.responsive) {
+    function renderResponsiveBottomRow(): JSX.Element {
+        if (props.responsive) {
             return (
                 <React.Fragment>
                     <button
                         className={`dtc-viewer-handle dtc-viewer-handle__bottom-left`}
-                        onMouseDown={this.handleMouseDown(
-                            ResizeHandleLocation.bottomLeft
-                        )}
+                        onMouseDown={handleMouseDown(ResizeHandleLocation.bottomLeft)}
                     />
                     <button
                         className={`dtc-viewer-handle dtc-viewer-handle__bottom`}
                         aria-hidden={true}
-                        onMouseDown={this.handleMouseDown(ResizeHandleLocation.bottom)}
+                        onMouseDown={handleMouseDown(ResizeHandleLocation.bottom)}
                     />
                     <button
                         className={`dtc-viewer-handle dtc-viewer-handle__bottom-right`}
-                        onMouseDown={this.handleMouseDown(
-                            ResizeHandleLocation.bottomRight
-                        )}
+                        onMouseDown={handleMouseDown(ResizeHandleLocation.bottomRight)}
                     />
                 </React.Fragment>
             );
         }
     }
 
-    private shouldUpdateResizingEventListeners(prevState: ViewerState): void {
-        if (this.state.resizing && !prevState.resizing) {
-            document.addEventListener("mouseup", this.handleMouseUp);
-            document.addEventListener("mousemove", this.handleMouseMove);
-        } else if (!this.state.resizing && prevState.resizing) {
-            document.removeEventListener("mouseup", this.handleMouseUp);
-            document.removeEventListener("mousemove", this.handleMouseMove);
-        }
-    }
-
-    private getHeight(): any {
-        if (this.props.height) {
+    function getHeight(): any {
+        if (props.height) {
             return {
-                height: `${this.props.height}px`,
+                height: `${props.height}px`,
             };
         }
     }
 
-    private getWidth(): any {
-        if (this.props.width) {
+    function getWidth(): any {
+        if (props.width) {
             return {
-                width: `${this.props.width}px`,
+                width: `${props.width}px`,
             };
         }
     }
 
-    private generateContentRegionClassNames(): string {
+    function generateContentRegionClassNames(): string {
         let classes: string = "dtc-viewer_content-region";
 
-        if (this.props.preview) {
+        if (props.preview) {
             classes += ` ${"dtc-viewer_content-region__preview"}`;
         }
 
-        if (this.state.resizing) {
+        if (resizing) {
             classes += ` ${"dtc-viewer_content-region__disabled"}`;
         }
 
         return classes;
     }
 
-    private postMessage(message: MessageEvent): void {
-        if (canUseDOM() && get(this.iframeRef, "current.contentWindow")) {
-            this.iframeRef.current.contentWindow.postMessage(
-                JSON.stringify(message),
-                "*"
-            );
+    function postMessage(message: MessageEvent): void {
+        if (canUseDOM() && get(iframeRef, "current.contentWindow")) {
+            iframeRef.current.contentWindow.postMessage(JSON.stringify(message), "*");
         }
     }
 
-    private handleIframeMessage = (e: MessageEvent): void => {
+    function handleIframeMessage(e: MessageEvent): void {
         if (
             e.data &&
             e.data.type === MessageSystemType.custom &&
             e.data.action === ViewerCustomAction.call
         ) {
             e.stopImmediatePropagation();
-            this.props.messageSystem.postMessage({
+            props.messageSystem.postMessage({
                 ...e.data,
                 action: ViewerCustomAction.response,
             });
         }
-    };
+    }
 
-    private handleMessageSystem = (e: MessageEvent): void => {
-        this.postMessage(e.data);
-    };
+    function handleMessageSystem(e: MessageEvent): void {
+        postMessage(e.data);
+    }
 
     /**
      * Handle mouseUp
      */
-    private handleMouseUp = (e: MouseEvent): void => {
+    function handleMouseUp(e: MouseEvent): void {
         // only listen for left click
         if (e.button !== 0) {
             return;
         }
 
-        this.setState({
-            resizing: false,
-            dragReferenceY: null,
-            dragReferenceX: null,
-            dragHandleLocation: null,
-        });
-    };
+        setResizing(false);
+        setDragReferenceY(null);
+        setDragReferenceX(null);
+        setDragHandleLocation(null);
+    }
 
-    private handleMouseMove = (e: MouseEvent): void => {
-        if (!this.state.resizing) {
+    function handleMouseMove(e: MouseEvent): void {
+        if (!resizing) {
             return;
         }
 
-        const heightOffset: number = this.state.dragReferenceY - e.pageY;
-        const widthOffset: number = (this.state.dragReferenceX - e.pageX) * 2;
-        const updatedHeight: number = this.props.height - heightOffset;
-        const updatedWidthLeft: number = this.props.width + widthOffset;
-        const updatedWidthRight: number = this.props.width - widthOffset;
+        const heightOffset: number = dragReferenceY - e.pageY;
+        const widthOffset: number = (dragReferenceX - e.pageX) * 2;
+        const updatedHeight: number = props.height - heightOffset;
+        const updatedWidthLeft: number = props.width + widthOffset;
+        const updatedWidthRight: number = props.width - widthOffset;
 
-        switch (this.state.dragHandleLocation) {
+        switch (dragHandleLocation) {
             case ResizeHandleLocation.bottom:
-                this.setState({
-                    dragReferenceY: e.pageY,
-                });
+                setDragReferenceY(e.pageY);
 
-                this.handleUpdateHeight(updatedHeight);
+                handleUpdateHeight(updatedHeight);
                 break;
             case ResizeHandleLocation.bottomLeft:
-                this.setState({
-                    dragReferenceY: e.pageY,
-                    dragReferenceX: e.pageX,
-                });
+                setDragReferenceY(e.pageY);
+                setDragReferenceX(e.pageX);
 
-                this.handleUpdateHeight(updatedHeight);
-                this.handleUpdateWidth(updatedWidthLeft);
+                handleUpdateHeight(updatedHeight);
+                handleUpdateWidth(updatedWidthLeft);
                 break;
             case ResizeHandleLocation.bottomRight:
-                this.setState({
-                    dragReferenceY: e.pageY,
-                    dragReferenceX: e.pageX,
-                });
+                setDragReferenceY(e.pageY);
+                setDragReferenceX(e.pageX);
 
-                this.handleUpdateHeight(updatedHeight);
-                this.handleUpdateWidth(updatedWidthRight);
+                handleUpdateHeight(updatedHeight);
+                handleUpdateWidth(updatedWidthRight);
                 break;
             case ResizeHandleLocation.left:
-                this.setState({
-                    dragReferenceX: e.pageX,
-                });
+                setDragReferenceX(e.pageX);
 
-                this.handleUpdateWidth(updatedWidthLeft);
+                handleUpdateWidth(updatedWidthLeft);
                 break;
             case ResizeHandleLocation.right:
-                this.setState({
-                    dragReferenceX: e.pageX,
-                });
+                setDragReferenceX(e.pageX);
 
-                this.handleUpdateWidth(updatedWidthRight);
+                handleUpdateWidth(updatedWidthRight);
                 break;
         }
-    };
+    }
 
-    private handleMouseDown = (
+    function handleMouseDown(
         handleLocation: ResizeHandleLocation
-    ): ((e: React.MouseEvent<HTMLButtonElement>) => void) => {
+    ): (e: React.MouseEvent<HTMLButtonElement>) => void {
         return (e: React.MouseEvent<HTMLButtonElement>): void => {
             // only listen for left click
             if (e.button !== 0) {
@@ -330,79 +205,89 @@ export class Viewer extends React.Component<
 
             switch (handleLocation) {
                 case ResizeHandleLocation.bottom:
-                    this.handleBottomMouseDown(e);
+                    handleBottomMouseDown(e);
                     break;
                 case ResizeHandleLocation.bottomRight:
-                    this.handleBottomRightMouseDown(e);
+                    handleBottomRightMouseDown(e);
                     break;
                 case ResizeHandleLocation.bottomLeft:
-                    this.handleBottomLeftMouseDown(e);
+                    handleBottomLeftMouseDown(e);
                     break;
                 case ResizeHandleLocation.left:
-                    this.handleLeftMouseDown(e);
+                    handleLeftMouseDown(e);
                     break;
                 case ResizeHandleLocation.right:
-                    this.handleRightMouseDown(e);
+                    handleRightMouseDown(e);
                     break;
             }
         };
-    };
+    }
 
-    private handleBottomMouseDown = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        this.setState({
-            resizing: true,
-            dragReferenceY: e.pageY,
-            dragHandleLocation: ResizeHandleLocation.bottom,
-        });
-    };
+    function handleBottomMouseDown(e: React.MouseEvent<HTMLButtonElement>): void {
+        setResizing(true);
+        setDragReferenceY(e.pageY);
+        setDragHandleLocation(ResizeHandleLocation.bottom);
+    }
 
-    private handleBottomRightMouseDown = (
-        e: React.MouseEvent<HTMLButtonElement>
-    ): void => {
-        this.setState({
-            resizing: true,
-            dragReferenceY: e.pageY,
-            dragReferenceX: e.pageX,
-            dragHandleLocation: ResizeHandleLocation.bottomRight,
-        });
-    };
+    function handleBottomRightMouseDown(e: React.MouseEvent<HTMLButtonElement>): void {
+        setResizing(true);
+        setDragReferenceY(e.pageY);
+        setDragReferenceX(e.pageX);
+        setDragHandleLocation(ResizeHandleLocation.bottomRight);
+    }
 
-    private handleBottomLeftMouseDown = (
-        e: React.MouseEvent<HTMLButtonElement>
-    ): void => {
-        this.setState({
-            resizing: true,
-            dragReferenceY: e.pageY,
-            dragReferenceX: e.pageX,
-            dragHandleLocation: ResizeHandleLocation.bottomLeft,
-        });
-    };
+    function handleBottomLeftMouseDown(e: React.MouseEvent<HTMLButtonElement>): void {
+        setResizing(true);
+        setDragReferenceY(e.pageY);
+        setDragReferenceX(e.pageX);
+        setDragHandleLocation(ResizeHandleLocation.bottomLeft);
+    }
 
-    private handleLeftMouseDown = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        this.setState({
-            resizing: true,
-            dragReferenceX: e.pageX,
-            dragHandleLocation: ResizeHandleLocation.left,
-        });
-    };
+    function handleLeftMouseDown(e: React.MouseEvent<HTMLButtonElement>): void {
+        setResizing(true);
+        setDragReferenceX(e.pageX);
+        setDragHandleLocation(ResizeHandleLocation.left);
+    }
 
-    private handleRightMouseDown = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        this.setState({
-            resizing: true,
-            dragReferenceX: e.pageX,
-            dragHandleLocation: ResizeHandleLocation.right,
-        });
-    };
+    function handleRightMouseDown(e: React.MouseEvent<HTMLButtonElement>): void {
+        setResizing(true);
+        setDragReferenceX(e.pageX);
+        setDragHandleLocation(ResizeHandleLocation.right);
+    }
 
-    private handleUpdateHeight(height: number): void {
-        if (this.props.onUpdateHeight) {
-            this.props.onUpdateHeight(height);
+    function handleUpdateHeight(height: number): void {
+        if (props.onUpdateHeight) {
+            props.onUpdateHeight(height);
         }
     }
 
-    private handleUpdateWidth(width: number): void {
-        if (this.props.onUpdateWidth) {
-            this.props.onUpdateWidth(width);
+    function handleUpdateWidth(width: number): void {
+        if (props.onUpdateWidth) {
+            props.onUpdateWidth(width);
         }
     }
+
+    return (
+        <div className={"dtc-viewer"}>
+            <div
+                className={generateContentRegionClassNames()}
+                style={{
+                    ...getHeight(),
+                    ...getWidth(),
+                }}
+            >
+                {renderResponsiveLeftHandle()}
+                <base target="_blank" />
+                <iframe
+                    ref={iframeRef}
+                    className={"dtc-viewer_iframe"}
+                    src={props.iframeSrc}
+                >
+                    Your browser does not support iframes.
+                </iframe>
+                {renderResponsiveRightHandle()}
+                {renderResponsiveBottomRow()}
+            </div>
+        </div>
+    );
 }
