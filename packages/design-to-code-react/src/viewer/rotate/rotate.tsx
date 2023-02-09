76e7b28abd535364ce2a929a1b7ab7a67c94d41a
janechu <7559015+janechu@users.noreply.github.com>
2023-02-08 19:32:16 -0800

    Convert rotate class component to function component

--- packages/design-to-code-react/src/viewer/rotate/rotate.tsx
+++ packages/design-to-code-react/src/viewer/rotate/rotate.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Rotate as BaseRotate } from "./rotate.base";
-import {
-    Orientation,
-    RotateHandledProps,
-    RotateProps,
-    RotateUnhandledProps,
-} from "./rotate.props";
+import { Orientation, RotateProps } from "./rotate.props";
 
 /*
  * The type returned by manageJss type is very complicated so we'll let the
  * compiler infer the type instead of re-declaring just for the package export
  */
 const Rotate = BaseRotate;
-type Rotate = InstanceType<typeof Rotate>;
 
-export { Orientation, Rotate, RotateProps, RotateHandledProps, RotateUnhandledProps };
+export { Orientation, Rotate, RotateProps };
