055cd0f0cad7b65ba4125e60f88246e90d4b8ac5
janechu <7559015+janechu@users.noreply.github.com>
2023-02-08 22:24:09 -0800

    Convert the file class component to a function

--- packages/design-to-code-react/src/form/custom-controls/control.file.tsx
+++ packages/design-to-code-react/src/form/custom-controls/control.file.tsx
@@ -9,46 +9,37 @@ import { fileComponent } from "design-to-code/dist/esm/web-components";
  * @alpha
  * Custom form control definition
  */
-class FileControl extends React.Component<
-    FileControlProps & { children: React.ReactNode }
-> {
-    public static displayName: string = "FileControl";
-
-    /**
-     * Render the component
-     */
-    public render(): React.ReactNode {
-        return (
-            <div
-                className={classNames("dtc-file-control", [
-                    "dtc-file-control__disabled",
-                    this.props.disabled,
-                ])}
-            >
-                <dtc-file
-                    accept={this.props.accept}
-                    events={{
-                        change: this.onChange,
-                    }}
-                    disabled={this.props.disabled}
-                >
-                    {this.props.children}
-                    <dtc-file-action-objecturl
-                        role="fileaction"
-                        slot="action"
-                    ></dtc-file-action-objecturl>
-                </dtc-file>
-            </div>
-        );
-    }
-
+function FileControl(props: FileControlProps & { children: React.ReactNode }) {
     /**
      * Callback for input change
      */
-    public onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    function onChange(e: React.ChangeEvent<HTMLInputElement>): void {
         const newValue: string = (e.target as fileComponent).fileReferences[0];
-        this.props.onChange({ value: newValue });
-    };
+        props.onChange({ value: newValue });
+    }
+
+    return (
+        <div
+            className={classNames("dtc-file-control", [
+                "dtc-file-control__disabled",
+                props.disabled,
+            ])}
+        >
+            <dtc-file
+                accept={props.accept}
+                events={{
+                    change: onChange,
+                }}
+                disabled={props.disabled}
+            >
+                {props.children}
+                <dtc-file-action-objecturl
+                    role="fileaction"
+                    slot="action"
+                ></dtc-file-action-objecturl>
+            </dtc-file>
+        </div>
+    );
 }
 
 export { FileControl };
