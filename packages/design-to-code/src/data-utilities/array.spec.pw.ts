d452305e72e2a2a9612f3ef855b4a931ce486542
janechu <7559015+janechu@users.noreply.github.com>
2023-02-24 22:08:51 -0800

    Added array tests

---
+++ packages/design-to-code/src/data-utilities/array.spec.pw.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "@playwright/test";
+import { isInArray } from "./array.js";
+
+/**
+ * Checks that the data is in an array
+ */
+test.describe.only("isInArray", () => {
+    test("should return false when data is not an array and the data location is the root", () => {
+        expect(isInArray({}, "")).toEqual(false);
+    });
+    test("should return false when data is not an array and the data location is in a sub location", () => {
+        expect(
+            isInArray(
+                {
+                    foo: {},
+                },
+                "foo"
+            )
+        ).toEqual(false);
+    });
+    test("should return true when data is an array and the data location is in a sub location", () => {
+        expect(isInArray(["foo"], "[0]")).toEqual(false);
+    });
+});
