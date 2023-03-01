6798e8d225430df3247473045d5b1525d578cc1f
janechu <7559015+janechu@users.noreply.github.com>
2023-02-28 20:06:50 -0800

    Added tests for the monaco adapter service class and utilities

--- packages/design-to-code/app/examples/message-system/index.ts
+++ packages/design-to-code/app/examples/message-system/index.ts
@@ -1,4 +1,11 @@
-import { AjvMapper, MessageSystem } from "design-to-code";
+import {
+    AjvMapper,
+    MessageSystem,
+    MonacoAdapter,
+    MonacoAdapterAction,
+} from "design-to-code";
+import { mapDataDictionaryToMonacoEditorHTML } from "design-to-code/dist/esm/data-utilities/monaco.js";
+import { monacoAdapterId } from "design-to-code/dist/esm/message-system-service/monaco-adapter.service.js";
 
 /**
  * These utilities are used for testing in browser
@@ -6,4 +13,8 @@ import { AjvMapper, MessageSystem } from "design-to-code";
 (window as any).dtc = {
     AjvMapper,
     MessageSystem,
+    MonacoAdapter,
+    MonacoAdapterAction,
+    mapDataDictionaryToMonacoEditorHTML,
+    monacoAdapterId,
 };
