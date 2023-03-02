dceb1a38c59c807916918d907fe1cae4d98029fd
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-03-02 15:03:25 -0800

    Add scaffolding for the Editor application with associated toolbar and integration into the documentation site (#81)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change adds:
    - scaffolding for the editor application with the documentation site toolbar
    - a link in the documentation toolbar to the editor
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Related to #79 
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.

---
+++ app/src/components/toolbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import toolbarTemplate from "static-docs/dist/esm/templates/toolbar/index.js";
+import staticDocsStyleTemplate from "static-docs/dist/esm/templates/style/index.js";
+import { template as templateResolver, escape, forEach } from "lodash-es";
+import { toolbarConfig } from "../../../build/toolbar-config.js";
+
+const compiledToolbarTemplate = templateResolver(toolbarTemplate, {
+    imports: {
+        // lodash imports needed for the compiler
+        _: {
+            forEach,
+            escape,
+        },
+    },
+});
+
+const compiledStaticDocsStyleTemplate = templateResolver(staticDocsStyleTemplate);
+
+function createMarkup() {
+    return {
+        __html: compiledToolbarTemplate(toolbarConfig),
+    };
+}
+
+function createStyleMarkup() {
+    return {
+        __html: compiledStaticDocsStyleTemplate(),
+    };
+}
+
+export function Toolbar() {
+    return (
+        <React.Fragment>
+            <div dangerouslySetInnerHTML={createStyleMarkup()} />
+            <div dangerouslySetInnerHTML={createMarkup()} />
+        </React.Fragment>
+    );
+}
