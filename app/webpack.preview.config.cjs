463b5fcdb03bbc1e653f80fa063dae18481a22ba
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-03-08 13:26:47 -0800

    Update pathing to make the example app work on gh-pages (#98)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    This change updates pathing to be absolute for the gh-pages production deployment.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Closes #79
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.

--- app/webpack.preview.config.cjs
+++ app/webpack.preview.config.cjs
@@ -3,75 +3,75 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { ghPagesBaseUrl } = require("../build/constants.js");
 
 const appDir = path.resolve(__dirname, "./preview");
 const outDir = path.resolve(__dirname, "./www/preview");
 
-module.exports = {
-    name: "preview",
-    entry: {
-        main: path.resolve(appDir, "index.ts"),
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    output: {
-        path: outDir,
-        publicPath: process.env.NODE_ENV === "production" ? "./editor/preview/" : "./preview/",
-        filename: "[name].[contenthash].js",
-    },
-    mode: process.env.NODE_ENV || "development",
-    module: {
-        // where we defined file patterns and their loaders
-        rules: [
-            {
-                test: /.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(svg|png|jpe?g|gif|ttf)$/i,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        esModule: false,
+module.exports = (env, argv) => {
+    return {
+        name: "preview",
+        entry: {
+            main: path.resolve(appDir, "index.ts"),
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        output: {
+            path: outDir,
+            publicPath: argv.mode === "production" ? `${ghPagesBaseUrl}/editor/preview/` : "/preview/",
+            filename: "[name].[contenthash].js",
+        },
+        mode: argv.mode || "development",
+        module: {
+            // where we defined file patterns and their loaders
+            rules: [
+                {
+                    test: /.tsx?$/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                        },
+                    ],
+                },
+                {
+                    test: /\.(svg|png|jpe?g|gif|ttf)$/i,
+                    use: {
+                        loader: "file-loader",
+                        options: {
+                            esModule: false,
+                        },
                     },
                 },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                ],
-            },
-            {
-                test: /message\-system\.min\.js/,
-                use: {
-                    loader: "worker-loader",
-                    options: {
-                        publicPath: "./",
-                    }
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: "css-loader",
+                        },
+                    ],
                 },
-            },
+                {
+                    test: /message\-system\.min\.js/,
+                    use: {
+                        loader: "worker-loader",
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                title: "Preview",
+                inject: "body",
+                template: path.resolve(appDir, "index.html"),
+                globalCssVariableStylesheet: argv.mode === "production"
+                    ? `${ghPagesBaseUrl}/editor/public/global.css-variables.css`
+                    : "/public/global.css-variables.css",
+            }),
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            title: "Preview",
-            inject: "body",
-            template: path.resolve(appDir, "index.html"),
-            globalCssVariableStylesheet: process.env.NODE_ENV === "production"
-                ? "/editor/public/global.css-variables.css"
-                : "/public/global.css-variables.css",
-        }),
-    ],
-};
+    };
+}
