ee3a049b735785cbd2703fcc23aa151538a9a08b
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-08-21 11:15:28 -0700

    Added an example of a function component using the Form component with the message system (#126)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    Lifecycle of React function components may be confusing when implementing the message system and the `Form` component, this is an example in the test app to illustrate how it might be accomplished.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.

---
+++ packages/design-to-code-react/app/pages/function-form.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { MessageSystem, getDataFromSchema, SchemaDictionary } from "design-to-code";
+import * as testConfigs from "./form/";
+import { Form } from "../../src";
+
+const exampleData: any = getDataFromSchema(testConfigs.objects.schema);
+
+function generateSchemaDictionary(): SchemaDictionary {
+    const schemaDictionary: SchemaDictionary = {};
+
+    Object.keys(testConfigs).forEach((testConfigKey: string) => {
+        schemaDictionary[testConfigs[testConfigKey].schema.$id] =
+            testConfigs[testConfigKey].schema;
+    });
+
+    return schemaDictionary;
+}
+
+const messageSystem = new MessageSystem({
+    webWorker: "message-system.js",
+    dataDictionary: [
+        {
+            foo: {
+                schemaId: testConfigs.objects.schema.$id,
+                data: exampleData,
+            },
+        },
+        "foo",
+    ],
+    schemaDictionary: generateSchemaDictionary(),
+});
+
+export function FunctionFormTestPage() {
+    const [data, setData] = useState(exampleData);
+    const [navigationConfigId, setNavigationConfigId] = useState("");
+
+    const handleMessageSystem = e => {
+        switch (e.data?.type) {
+            case "data":
+                setData(e.data.data);
+            default:
+                setNavigationConfigId(e.data.activeNavigationConfigId);
+        }
+    };
+
+    useEffect(() => {
+        const messageSystemConfig = {
+            onMessage: handleMessageSystem,
+        };
+        messageSystem.add(messageSystemConfig);
+
+        return () => {
+            messageSystem.remove(messageSystemConfig);
+        };
+    }, []);
+
+    return (
+        <div>
+            <span>{navigationConfigId}</span>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <Form messageSystem={messageSystem} />
+        </div>
+    );
+}
