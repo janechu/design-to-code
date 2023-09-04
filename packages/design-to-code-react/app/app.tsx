65370abdcf76f3c8b70e0dfa33535574cafbbfe2
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-09-04 10:38:33 -0700

    Updated the form test page and use search params for future Playwright snapshots (#139)
    # Pull Request
    
    ## 📖 Description
    
    <!--- Provide some background and a description of your work. -->
    In preparation for the addition of Playwright snapshot tests the test app has been updated/
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Starts work on #137
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->
    - Add snapshot tests for each JSON schema, update schemas as necessary

--- packages/design-to-code-react/app/app.tsx
+++ packages/design-to-code-react/app/app.tsx
@@ -1,76 +1,37 @@
 import React from "react";
-import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { NavigationTestPage } from "./pages/navigation";
 import ViewerPage from "./pages/viewer";
 import ViewerContentPage from "./pages/viewer/content";
-import { FormTestPage } from "./pages/form";
 import { FormAndNavigationTestPage } from "./pages/form-and-navigation";
 import { WebComponentTestPage } from "./pages/web-components";
 import WebComponentViewerContent from "./pages/web-components/web-component-viewer-content";
-import { FunctionFormTestPage } from "./pages/function-form";
+import { FormTestPage } from "./pages/form";
+import { IndexPage } from "./pages/index";
 
 class App extends React.Component<{}, {}> {
     public render(): React.ReactNode {
         return (
             <BrowserRouter>
-                <div>
-                    {this.renderLinks()}
-                    <Routes>
-                        <Route path={"/navigation"} element={<NavigationTestPage />} />
-                        <Route path={"/viewer"} element={<ViewerPage />} />
-                        <Route path={"/viewer/content"} element={<ViewerContentPage />} />
-                        <Route path={"/form"} element={<FormTestPage />} />
-                        <Route
-                            path={"/function-form"}
-                            element={<FunctionFormTestPage />}
-                        />
-                        <Route
-                            path={"/form-and-navigation"}
-                            element={<FormAndNavigationTestPage />}
-                        />
-                        <Route
-                            path={"/web-components"}
-                            element={<WebComponentTestPage />}
-                        />
-                        <Route
-                            path={"/web-components/content"}
-                            element={<WebComponentViewerContent />}
-                        />
-                        <Route path={"/"} element={<Navigate to={"/form"} />} />
-                    </Routes>
-                </div>
+                <Routes>
+                    <Route path={"/"} element={<IndexPage />} />
+                    <Route path={"/navigation"} element={<NavigationTestPage />} />
+                    <Route path={"/viewer"} element={<ViewerPage />} />
+                    <Route path={"/viewer/content"} element={<ViewerContentPage />} />
+                    <Route path={"/form"} element={<FormTestPage />} />
+                    <Route
+                        path={"/form-and-navigation"}
+                        element={<FormAndNavigationTestPage />}
+                    />
+                    <Route path={"/web-components"} element={<WebComponentTestPage />} />
+                    <Route
+                        path={"/web-components/content"}
+                        element={<WebComponentViewerContent />}
+                    />
+                    <Route path={"/"} element={<Navigate to={"/form"} />} />
+                </Routes>
             </BrowserRouter>
         );
-    }
-
-    private renderLinks(): React.ReactNode {
-        if (window.location.pathname.slice(-7) !== "content") {
-            return (
-                <React.Fragment>
-                    <ul>
-                        <li>
-                            <Link to="/form">Form</Link>
-                        </li>
-                        <li>
-                            <Link to="/function-form">Function Form</Link>
-                        </li>
-                        <li>
-                            <Link to="/navigation">Navigation</Link>
-                        </li>
-                        <li>
-                            <Link to="/form-and-navigation">Form and Navigation</Link>
-                        </li>
-                        <li>
-                            <Link to="/viewer">Viewer</Link>
-                        </li>
-                        <li>
-                            <Link to="/web-components">Web Components</Link>
-                        </li>
-                    </ul>
-                    <hr />
-                </React.Fragment>
-            );
-        }
     }
 }
 
