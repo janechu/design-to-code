08d0fd94bd8046cea16937d5f16cfeacbb15a46d
Jane Chu <7559015+janechu@users.noreply.github.com>
2023-09-16 15:06:25 -0700

    Add button control playwright tests (#163)
    # Pull Request
    
    ## 📖 Description
    
    Adds tests for the button control.
    
    ### 🎫 Issues
    
    <!---
    List and link relevant issues here using the keyword "closes"
    if this PR will close an issue, eg. closes #411
    -->
    Continues work on #133
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [x] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.

---
+++ packages/design-to-code-react/src/form/controls/control.button.spec.pw.snapshot.ts
@@ -0,0 +1,38 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("button", () => {
+    test.describe("snapshot", () => {
+        test("base", async ({ page }) => {
+            await page.goto("/form?schema=controlButton");
+            await expect(page).toHaveScreenshot();
+        });
+        test.skip("default", async ({ page }) => {
+            // build gate is failing on this
+            await page.goto("/form?schema=controlButtonDefault");
+            await expect(page).toHaveScreenshot();
+        });
+        test("disabled", async ({ page }) => {
+            await page.goto("/form?schema=controlButtonDisabled");
+            await expect(page).toHaveScreenshot();
+        });
+        test.describe("invalid", () => {
+            test("default", async ({ page }) => {
+                await page.goto("/form?schema=controlButtonInvalid");
+                await expect(page).toHaveScreenshot();
+            });
+            test("blur", async ({ page }) => {
+                await page.goto("/form?schema=controlButtonInvalid");
+                await page.waitForSelector(".dtc-form .dtc-button-control");
+                await page.locator(".dtc-form .dtc-button-control").focus();
+                await page.locator(".dtc-form .dtc-button-control").blur();
+                await expect(page).toHaveScreenshot();
+            });
+            test("inline", async ({ page }) => {
+                await page.goto(
+                    "/form?schema=controlButtonInvalid&displayValidationInline=true"
+                );
+                await expect(page).toHaveScreenshot();
+            });
+        });
+    });
+});
