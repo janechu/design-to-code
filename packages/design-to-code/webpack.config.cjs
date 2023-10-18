f373be24ea1c417c686f3fe6a39abca6441e346a
janechu <7559015+janechu@users.noreply.github.com>
2023-10-18 11:26:33 -0700

    Update dependencies and remove jest configuration

--- packages/design-to-code/webpack.config.cjs
+++ packages/design-to-code/webpack.config.cjs
@@ -1,37 +1,28 @@
 const path = require("path");
-const glob = require("glob");
+const { globSync } = require("glob");
 const rootAppWebpackConfig = require(path.resolve(__dirname, "app/webpack.dev.cjs"));
 const outDir = path.resolve(__dirname, "./www");
 
 const configs = new Promise(resolver => {
     try {
-        glob(
-            `${path.resolve(__dirname, "./app/examples")}/*/webpack.dev.cjs`,
-            {
-                mode: "development",
-            },
-            function (err, files) {
-                if (err) {
-                    throw err;
-                }
+        const files = globSync(
+            `${path.resolve(__dirname, "./app/examples")}/*/webpack.dev.cjs`);
 
-                resolver(
-                    files.map((file, index) => {
-                        const webpackConfig = require(file);
-                        const name = path.basename(path.dirname(file));
+            resolver(
+                files.map((file, index) => {
+                    const webpackConfig = require(file);
+                    const name = path.basename(path.dirname(file));
 
-                        webpackConfig.name = name;
-                        webpackConfig.output = {
-                            ...webpackConfig.output,
-                            path: path.resolve(outDir, name),
-                            publicPath: `/${name}/`,
-                        };
+                    webpackConfig.name = name;
+                    webpackConfig.output = {
+                        ...webpackConfig.output,
+                        path: path.resolve(outDir, name),
+                        publicPath: `/${name}/`,
+                    };
 
-                        return webpackConfig;
-                    })
-                );
-            }
-        );
+                    return webpackConfig;
+                })
+            );
     } catch (err) {
         console.info(`No example app webpack files found`, err);
         process.exit(0);
