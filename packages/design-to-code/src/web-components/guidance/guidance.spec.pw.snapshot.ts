c72060d04e4a7da9afc707fc125dd64f70361982
janechu <7559015+janechu@users.noreply.github.com>
2023-10-24 19:54:21 -0700

    Update snapshots

--- packages/design-to-code/src/web-components/guidance/guidance.spec.pw.snapshot.ts
+++ packages/design-to-code/src/web-components/guidance/guidance.spec.pw.snapshot.ts
@@ -10,9 +10,13 @@ test.describe("guidance", () => {
         test("document selected", async ({ page }) => {
             await page.goto("/guidance");
             await page.locator("#guidance").waitFor({ state: "visible" });
+
+            const filter = await page.locator("#guidance input");
+            await filter.focus();
+
             const listItem = await page.locator("#guidance .list li").nth(0);
 
-            await listItem.locator("a").click();
+            await (await listItem.locator("a")).click();
             await expect(page).toHaveScreenshot();
         });
         test("documents filtered", async ({ page }) => {
