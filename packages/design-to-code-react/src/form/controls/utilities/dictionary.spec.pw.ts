e8fde2cfd5a89d68c5f3f55719e8c8fdba09dadf
Jane Chu <7559015+janechu@users.noreply.github.com>
2024-03-21 10:18:24 -0700

    Add tests

--- packages/design-to-code-react/src/form/controls/utilities/dictionary.spec.pw.ts
+++ packages/design-to-code-react/src/form/controls/utilities/dictionary.spec.pw.ts
@@ -1,33 +1,77 @@
 import { expect, test } from "../../../__tests__/base-fixtures.js";
 
 test.describe("KeywordAdditionalProperties", () => {
-    test("should generate controls if additionalProperties is an object with type specified", async ({
+    test("should generate an add control if additionalProperties is an object with type is specified", async ({
         page,
     }) => {
         await page.goto("/form?schema=keywordAdditionalPropertiesAsObject");
 
-        await page.waitForSelector(".dtc-form a.dtc-dictionary");
+        await page.waitForSelector(".dtc-form .dtc-dictionary_control-region");
 
-        const dictionary = await page.locator(".dtc-form a.dtc-dictionary");
+        const dictionary = await page.locator(".dtc-form .dtc-dictionary_control-region");
 
         await expect(await dictionary.count()).toEqual(1);
+
+        await expect(
+            await dictionary.locator("button.dtc-dictionary_control-add-trigger").count()
+        ).toEqual(1);
     });
-    // test("should generate controls if additionalProperties is true", async ({ page }) => {
-    //     await page.goto("/form?schema=keywordAdditionalPropertiesAsTrue");
+    test("should add a control if the object with type is specified", async ({
+        page,
+    }) => {
+        await page.goto("/form?schema=keywordAdditionalPropertiesAsObject");
 
-    //     await page.waitForSelector(".dtc-form a.dtc-section-link-control");
+        await page.waitForSelector(".dtc-form .dtc-dictionary_control-region");
 
-    //     const link = await page.locator(".dtc-form a.dtc-section-link-control");
+        const dictionaryItems = page.locator(".dtc-dictionary_item-control-region");
 
-    //     await expect(await link.count()).toEqual(1);
-    // });
-    // test("should not generate controls if additionalProperties is false", async ({ page }) => {
-    //     await page.goto("/form?schema=keywordAdditionalPropertiesAsFalse");
+        await expect(await dictionaryItems.count()).toEqual(0);
 
-    //     await page.waitForSelector(".dtc-form a.dtc-section-link-control");
+        await page.locator("button.dtc-dictionary_control-add-trigger").click();
 
-    //     const link = await page.locator(".dtc-form a.dtc-section-link-control");
+        await expect(await dictionaryItems.count()).toEqual(1);
+    });
+    test("should generate an add control if additionalProperties is true", async ({
+        page,
+    }) => {
+        await page.goto("/form?schema=keywordAdditionalPropertiesAsTrue");
 
-    //     await expect(await link.count()).toEqual(1);
-    // });
+        await page.waitForSelector(".dtc-form .dtc-dictionary_control-region");
+
+        const dictionary = await page.locator(".dtc-form .dtc-dictionary_control-region");
+
+        await expect(await dictionary.count()).toEqual(1);
+
+        await expect(
+            await dictionary.locator("button.dtc-dictionary_control-add-trigger").count()
+        ).toEqual(1);
+    });
+    test("should add an untyped control if additionalProperties is true", async ({
+        page,
+    }) => {
+        await page.goto("/form?schema=keywordAdditionalPropertiesAsTrue");
+
+        await page.waitForSelector(".dtc-form .dtc-dictionary_control-region");
+
+        const dictionaryItems = page.locator(".dtc-dictionary_item-control-region");
+
+        await expect(await dictionaryItems.count()).toEqual(0);
+
+        await page.locator("button.dtc-dictionary_control-add-trigger").click();
+
+        await expect(await dictionaryItems.count()).toEqual(1);
+
+        await expect(await page.locator(".dtc-untyped-control").count()).toEqual(1);
+    });
+    test("should not generate controls if additionalProperties is false", async ({
+        page,
+    }) => {
+        await page.goto("/form?schema=keywordAdditionalPropertiesAsFalse");
+
+        const dictionaryControl = await page.locator(
+            ".dtc-form .dtc-dictionary_control-region"
+        );
+
+        await expect(await dictionaryControl.count()).toEqual(0);
+    });
 });
