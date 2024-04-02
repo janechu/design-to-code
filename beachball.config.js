12586e3e5083dab5b08f74c0e024eacb114aca54
Jane Chu <7559015+janechu@users.noreply.github.com>
2024-04-02 08:48:58 -0700

    Bump related packages together (#260)
    # Pull Request
    
    ## 📖 Description
    
    Sets the `design-to-code` and `design-to-code-react` packages to version together. This will make documentation easier.

--- beachball.config.js
+++ beachball.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     disallowedChangeTypes: ["major"],
+    bumpDeps: true,
+    groups: [
+        {
+            name: "dtc",
+            include: ["packages/design-to-code", "packages/design-to-code-react"],
+        },
+    ],
 };
